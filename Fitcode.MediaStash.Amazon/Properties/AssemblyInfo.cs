02fd775b5fa8c8c82072c01c821dd9811a59b925
Felipe Ramos <feliperamos86@gmail.com>
2017-01-01 01:16:59 -0600

    Initial Commit
    Completed preliminary work for Azure.

---
+++ Fitcode.MediaStash.Amazon/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fitcode.MediaStash.Amazon")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8152a4f6-ab8e-42a9-8b00-b93fdade8609")]
---
+++ Fitcode.MediaStash.Azure/MediaRepository.cs
@@ -0,0 +1,120 @@
+﻿#region License
+// Copyright (c) 2017 Fitcode.io
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+
+using Fitcode.MediaStash.Lib.Contracts;
+using Fitcode.MediaStash.Lib.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Fitcode.MediaStash.Azure
+{
+    public class MediaRepository : IMediaRepository
+    {
+        private readonly CloudStorageAccount _storageAccount;
+        private readonly CloudBlobClient _blobClient;
+
+        public MediaRepository(IRepositoryConfiguration config)
+        {
+            this.Config = config;
+
+            _storageAccount = CloudStorageAccount.Parse(config.ConnectionString);
+            _blobClient = _storageAccount.CreateCloudBlobClient();
+        }
+
+        public IRepositoryConfiguration Config { get; private set; }
+
+        public async Task Persist(IMediaContainer mediaContainer)
+        {
+            await Persist(mediaContainer, Config.RootContainer);
+        }
+
+        public async Task Persist(IMediaContainer mediaContainer, string storageContainer)
+        {
+            CloudBlobContainer rootContainer = _blobClient.GetContainerReference(storageContainer);
+
+            if (await rootContainer.ExistsAsync())
+            {
+                foreach (var file in mediaContainer.Media)
+                {
+                    CloudBlockBlob blob = rootContainer.GetBlockBlobReference($@"{mediaContainer.Path}\{file.Name}");
+
+                    await blob.UploadFromByteArrayAsync(file.Data, 0, file.Data.Length);
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException($"Container <{Config.RootContainer}> does not exist.");
+            }
+        }
+
+        public async Task<IMediaContainer> Retrieve(string path)
+        {
+            return await Retrieve(path, Config.RootContainer);
+        }
+
+        public async Task<IMediaContainer> Retrieve(string path, string storageContainer)
+        {
+            CloudBlobContainer rootContainer = _blobClient.GetContainerReference(storageContainer);
+
+            if (await rootContainer.ExistsAsync())
+            {
+                string prefix = $@"{path.Replace(@"\", "/")}/";
+
+                BlobResultSegment segment = await rootContainer.ListBlobsSegmentedAsync(prefix, new BlobContinuationToken());
+                var container = new MemoryMediaContainer
+                {
+                    Path = path
+                };
+                var media = new List<MemoryStreamMedia>();
+
+                foreach (IListBlobItem item in segment.Results)
+                {
+                    if (item.GetType() == typeof(CloudBlockBlob))
+                    {
+                        CloudBlockBlob blockBlob = item as CloudBlockBlob;
+                        var file = new MemoryStream();
+
+                        await blockBlob.DownloadToStreamAsync(file);
+
+                        media.Add(new MemoryStreamMedia(Path.GetFileName(blockBlob.Name), file));
+                    }
+                }
+
+                container.Media = media;
+                
+                return container;
+            }
+            else
+            {
+                throw new InvalidOperationException($"Container <{Config.RootContainer}> does not exist.");
+            }
+        }
+    }
+}
