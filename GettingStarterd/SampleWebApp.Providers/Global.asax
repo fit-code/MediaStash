2f75f5f232425d0ce6bb464938e8a87b967a27ae
Felipe Ramos <feliperamos86@gmail.com>
2017-01-15 00:07:54 -0600

    Sample Project
    -Added new providers sample project.

---
+++ GettingStarterd/SampleWebApp.Providers/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleWebApp.Providers.MvcApplication" Language="C#" %>
---
+++ GettingStarterd/SampleWebApp.Providers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleWebApp.Providers.Startup))]
+namespace SampleWebApp.Providers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
